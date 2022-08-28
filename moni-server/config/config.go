1ed017868d1105071a431f6cc309a9f43b2f95c7
liudong <1076624325@qq.com>
2022-08-28 18:36:38 +0800

    add: 修改配置文件校验

--- moni-server/config/config.go
+++ moni-server/config/config.go
@@ -2,8 +2,8 @@ package config
 
 type Server struct {
 	TaskSchedule map[string]string `json:"task_schedule" validate:"required"`
-	ServerHost   string            `json:"server_host" validate:"required"`
-	ServerPort   int               `json:"server_port" validate:"required"`
+	ServerHost   string            `json:"server_host" validate:"required,ip4_addr"`
+	ServerPort   int               `json:"server_port" validate:"required,gt=0"`
 	Log          Log               `json:"log" validate:"required"`
 	Mysql        Mysql             `json:"mysql" validate:"required"`
 	Redis        Redis             `json:"redis" validate:"required"`
@@ -12,24 +12,24 @@ type Server struct {
 
 // Mysql配置
 type Mysql struct {
-	MysqlHost     string `json:"mysql_host" validate:"required"`
-	MysqlPort     int    `json:"mysql_port" validate:"required"`
+	MysqlHost     string `json:"mysql_host" validate:"required,ip4_addr|hostname"`
+	MysqlPort     int    `json:"mysql_port" validate:"required,gt=0"`
 	MysqlUser     string `json:"mysql_user" validate:"required"`
 	MysqlPassword string `json:"mysql_password" validate:"required"`
 	DBName        string `json:"db_name" validate:"required"`
-	MaxIdleConns  int    `json:"max_idle_conns" validate:"required"`
-	MaxOpenConns  int    `json:"max_open_conns" validate:"required"`
+	MaxIdleConns  int    `json:"max_idle_conns" validate:"required,gt=0"`
+	MaxOpenConns  int    `json:"max_open_conns" validate:"required,gt=0"`
 }
 
 type Redis struct {
-	RedisHost      string `json:"redis_host" validate:"required"`
-	RedisPort      int    `json:"redis_port" validate:"required"`
+	RedisHost      string `json:"redis_host" validate:"required,ip4_addr|hostname"`
+	RedisPort      int    `json:"redis_port" validate:"required,gt=0"`
 	RedisPassword  string `json:"redis_password" validate:"required"`
-	PoolMaxIdle    int    `json:"pool_max_idle" validate:"required"`
-	PoolMaxActive  int    `json:"pool_max_active" validate:"required"`
-	RedisDB        int    `json:"redis_db" validate:"required"`
-	ConnectTimeOut int    `json:"connect_time_out" validate:"required"`
-	IdleTimeout    int    `json:"idle_timeout" validate:"required"`
+	PoolMaxIdle    int    `json:"pool_max_idle" validate:"required,gt=0"`
+	PoolMaxActive  int    `json:"pool_max_active" validate:"required,gt=0"`
+	RedisDB        int    `json:"redis_db" validate:"required,lte=15,gte=0"`
+	ConnectTimeOut int    `json:"connect_time_out" validate:"required,gt=0"`
+	IdleTimeout    int    `json:"idle_timeout" validate:"required,gt=0"`
 }
 type Log struct {
 	LogLevel string `json:"log_level" validate:"required"`
